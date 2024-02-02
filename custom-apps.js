fcba41617696fc3edb3481cdced6a5d6c852ca4c
kbstt <80958930+kbstt@users.noreply.github.com>
2024-02-02 10:11:02 +0800

    Update custom-apps.js

--- custom-apps.js
+++ custom-apps.js
@@ -3,15 +3,11 @@ let apps = [
   {slug: "lucas-de-poap-studio-1", id: "bTEFeQ3CSRb33drUhNEO"},    
   {slug: "mahou-experience-1", id: "uvQShCUSJLFwsOjiq55t"}   
 ];   
-console.log(apps);
 //your list of apps goes above
 let currentSlug = window.location.pathname.split("/")[2] || ""; 
 let currentApp = apps.find(o => o.slug === currentSlug) || apps[0];   
 let appId = currentApp.id;   
-console.log(appId);
 let script = document.createElement("script");   
 let scriptUrl = "https://progressier.app/"+appId+"/script.js";   
-console.log(scriptUrl);
 script.setAttribute("src", scriptUrl);   
 document.querySelector('body').appendChild(script);
-console.log(script);
