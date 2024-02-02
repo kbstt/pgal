3977b835be4ee272aa41be17d59b89af762bdb1c
cuchipando <158587372+cuchipando@users.noreply.github.com>
2024-02-02 14:29:04 +0000

    Update custom-apps.js

--- custom-apps.js
+++ custom-apps.js
@@ -1,7 +1,7 @@
 //your list of apps goes below
 let apps = [    
-  {slug: "lucas-de-poap-studio-1", id: "bTEFeQ3CSRb33drUhNEO"},    
-  {slug: "mahou-experience-1", id: "uvQShCUSJLFwsOjiq55t"}   
+  {slug: "version-0o3u/collection/lucas/lucas-de-poap-studio-1/", id: "bTEFeQ3CSRb33drUhNEO"},    
+  {slug: "version-0o3u/collection/mahou/mahou-experience-1/", id: "uvQShCUSJLFwsOjiq55t"}   
 ];   
 //your list of apps goes above
 let currentSlug = window.location.pathname.split("/")[2] || ""; 
